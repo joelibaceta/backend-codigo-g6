f2fb9f1e5a1f048753947cf18e37c95e04c9df02
Joel Ibaceta <mail@joelibaceta.com>
2022-04-14 22:00:11 -0500

    sesion 9

---
+++ sesion9/news/core/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+# Create your views here.
